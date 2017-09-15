78c9a729f2d1813b4ea18bde3135c4efdcddb881
ruanwenjun <541084008@qq.com>
2017-09-15 16:55:32 +0800

    修正L=D-W的错误

--- src/RecommendPath/SCluster.java
+++ src/RecommendPath/SCluster.java
@@ -24,6 +24,14 @@ public class SCluster {
                 W[i][j] = EditDistance.similarity(data.get(i).getData(), data.get(j).getData());
             }
         }
+//        //输出相似矩阵W
+//        System.out.println("输出W矩阵");
+//        for (int i = 0; i < len; i++) {
+//            for (int j = 0; j < len; j++) {
+//                System.out.printf("%2f ",W[i][j]);
+//            }
+//            System.out.println();
+//        }
         //把 W 的每一列元素加起来得到 N 个数，把它们放在对角线上（其他地方都是零），组成一个 N*N的矩阵，记为 D 。
         double[] D = new double[W.length];
         for (int i = 0; i < W.length; i++) {
@@ -36,8 +44,24 @@ public class SCluster {
         // L = D-W
         double[][] L = W.clone();
         for (int i = 0; i < W.length; i++) {
-            L[i][i] = L[i][i] - D[i];
+            for (int j = 0; j < W.length; j++) {
+                if (i == j)
+                {
+                    L[i][j] = D[i] - L[i][j];
+                }
+                else {
+                    L[i][j] = -L[i][j];
+                }
+            }
         }
+        //输出L矩阵
+//        System.out.println("输出L矩阵");
+//        for (int i = 0; i < len; i++) {
+//            for (int j = 0; j < len; j++) {
+//                System.out.printf("%2f ",L[i][j]);
+//            }
+//            System.out.println();
+//        }
         //求出 L 的前 k 个特征值（在本文中，除非特殊说明，否则“前 k 个”指按照特征值的大小从小到大的顺序）
         // \{\lambda\}_{i=1}^k 以及对应的特征向量 \{\mathbf{v}\}_{i=1}^k 。引用jama包
         Matrix L_matrix = new Matrix(L);
